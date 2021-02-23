061e5b0321d3f6599c05b1c92926ca73934f52f9
mittelmg <69327266+mittelmg@users.noreply.github.com>
2021-02-23 14:42:30 -0500

    Update lengthmod.rs

--- src/ciphers/schedulers/lengthmod.rs
+++ src/ciphers/schedulers/lengthmod.rs
@@ -7,12 +7,30 @@ use super::KeySchedule;
 
 impl KeySchedule for Lengthmod {
     fn schedule(&self, index: usize, key_length: usize, _plaintext_length: usize) -> usize {
-        if _plaintext_length< (index*key_length){
+         if _plaintext_length< (index*key_length){
             let mut returnInd = _plaintext_length % key_length;
             returnInd
         }else{
-            let mut returnInd = _plaintext_length % key_length;
+            let mut returnInd =(_plaintext_length*index) % key_length;
             returnInd
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    //tests the first branch option, if the resulting index is 0, its working correctly
+    fn test_lengthmod(){
+        let mut res = lengthmod.schedule(12, 50, 500);
+        assert_eq!(res, 0);
+    }
+
+    #[test]
+    //tests the second branch option, if the resulting index is 1, its working correctly
+    fn test_lengthmod(){
+        let mut res = lengthmod.schedule(1, 50, 501);
+        assert_eq!(res, 1);
+    }
