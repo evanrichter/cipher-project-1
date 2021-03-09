549799602ec57edd2afdfc8d07ec51ccb8504192
mittelmg <69327266+mittelmg@users.noreply.github.com>
2021-03-08 19:26:23 -0600

    Final edit - test complete
    This one should work.

--- src/crack/spellcheck.rs
+++ src/crack/spellcheck.rs
@@ -19,16 +19,16 @@ use crate::{Dictionary, utils::str_to_bytes, utils::bytes_to_str};
 pub fn spellcheck(cracked: &CrackResult, dict: &Dictionary) -> CrackResult {
 
     // do stuff here to create a real spell checked result
-    let mut correcttext: String; 
-    let iteratortext: String;
+    let mut correcttext: String; //the string we will correct
+    let iteratortext: String; //this is used on the for loop as an iterator
   
 
-
+    //convert from bytes to str
     iteratortext = bytes_to_str(&cracked.plaintext);
 
     correcttext = bytes_to_str(&cracked.plaintext).to_string();
     
-
+    //compare words to dict and correct inaccuracies
    for word in iteratortext.split_whitespace()//split_whitespace chunks the string out into words and returns an iterator
    {
     let dictMatch = dict.best_levenshtein(word); //finds the closest word match between the dict and our plaintext result
@@ -39,21 +39,20 @@ pub fn spellcheck(cracked: &CrackResult, dict: &Dictionary) -> CrackResult {
     }
     else //if it isn't, we make a change
     {
-       
         correcttext= correcttext.replace(word, dictMatch.0 );
-
     }
    }
 
+   //caclulate the confidence
 let numberofdif = levenshtein( &bytes_to_str(&cracked.plaintext), &correcttext) as f64;//finds the number of changes made between the corrected and origional
-
 let newconfidence: f64 = numberofdif/cracked.plaintext.len() as f64; //takes the number of levenstein differences over hte length. THis is the ratio between changes made and the length of the text. the higher the value the lower the confidence
  
+//create the output
 let errorcorrect =CrackResult{
     plaintext: str_to_bytes(&correcttext),
     confidence: newconfidence,
 };
-
+//final result
     errorcorrect
    
 }
@@ -73,9 +72,10 @@ let errorcorrect =CrackResult{
             };
 
             #[allow(dead_code)]
-            let mut newvect: &str= ["wordss", "wishes", "this", "pig", "the", "quics", "brown", "fox", "jumpede", "over", "the", "lazy", "dog", "cat", "lion", "seal", "fish", "canary", "sf", "f", "a", "fash", "carp", "sharks"];
+            //let mut newvect: &str= ["wordss", "wishes", "this", "pig", "the", "quics", "brown", "fox", "jumpede", "over", "the", "lazy", "dog", "cat", "lion", "seal", "fish", "canary", "sf", "f", "a", "fash", "carp", "sharks"];
+            let mut newstring=String::from("wordss wishes this pig the quics brown fox jumpede over the lazy dog cat lion seal fish canary sf f a fish carp sharks");
             let mut cracked= CrackResult{
-                plaintext: str_to_bytes(newvect),
+                plaintext: str_to_bytes(&newstring),
                 confidence: 4000.0,
             };
 
