f3b43c9c615f1cc0153baca100872a137f893293
amp813 <amp813@nyu.edu>
2021-03-20 14:46:09 -0400

    added main function to read ciphertext and return cracked plaintext

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,19 @@ use crack::crack_single_ciphertext;
 
 fn main() -> anyhow::Result<()> {
     // 1. get ciphertext from stdin
+    println!("Enter the ciphertext followed by a newline:");
+
+    // read one line from stdin
+    let stdin = std::io::stdin();
+    let mut ciphertext = String::new();
+    stdin.read_line(&mut ciphertext)?;
 
     // 2. crack ciphertext with crack_single_ciphertext()
+    let plaintext = crack_single_ciphertext(&ciphertext);
 
     // 3. print our plaintext guess on stdout
+    println!("Resulting plaintext is:\n");
+    println!("{}", plaintext);
 
     Ok(())
 }
